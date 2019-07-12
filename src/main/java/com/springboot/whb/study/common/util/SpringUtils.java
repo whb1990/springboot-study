ebf3119aaba31ea73fe7f6bb35ffb3ff29c02f8d
wanghongbo <270028806@qq.com>
2019-07-12 10:55:06 +0800

    Spring工具类

---
+++ src/main/java/com/springboot/whb/study/common/util/SpringUtils.java
@@ -0,0 +1,66 @@
+package com.springboot.whb.study.common.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @author: whb
+ * @date: 2019/7/12 10:49
+ * @description: Spring工具类
+ */
+public class SpringUtils implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtils.applicationContext == null) {
+            SpringUtils.applicationContext = applicationContext;
+        }
+        System.out.println("============ApplicationContext配置成功，在普通类可以通过调用SpringUitl.getApplicationContext()获取applicationContext对象=" + SpringUtils.applicationContext + "=============================================");
+    }
+
+    /**
+     * 获取ApplicationContext对象
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过Bean的name获取Bean
+     *
+     * @param beanName Bean名称
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String beanName) {
+        return (T) getApplicationContext().getBean(beanName);
+    }
+
+    /**
+     * 通过class获取Bean
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过beanName和class路径获取Bean
+     *
+     * @param beanName
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String beanName, Class<T> clazz) {
+        return getApplicationContext().getBean(beanName, clazz);
+    }
+}
