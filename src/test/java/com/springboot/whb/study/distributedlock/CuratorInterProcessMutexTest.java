39f96ad222ee876bbc9f834446a6370b87a6c6ce
wanghongbo <270028806@qq.com>
2019-07-15 17:03:32 +0800

    修改Curator可重入锁测试类；

--- src/test/java/com/springboot/whb/study/distributedlock/CuratorInterProcessMutexTest.java
+++ src/test/java/com/springboot/whb/study/distributedlock/CuratorInterProcessMutexTest.java
@@ -46,20 +46,17 @@ public class CuratorInterProcessMutexTest {
      */
     private static void generatorOrderNoWithoutLock() {
         for (int i = 0; i < 30; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        //阻塞
-                        countDownLatch.await();
-                        //获取锁
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
-                    String orderNo = sdf.format(new Date());
-                    System.out.println("不加锁生成的订单号是：" + orderNo);
+            new Thread(() -> {
+                try {
+                    //阻塞
+                    countDownLatch.await();
+                    //获取锁
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+                String orderNo = sdf.format(new Date());
+                System.out.println("不加锁生成的订单号是：" + orderNo);
             }).start();
             //唤醒
             countDownLatch.countDown();
@@ -73,27 +70,24 @@ public class CuratorInterProcessMutexTest {
         client.start();
         final InterProcessMutex lock = new InterProcessMutex(client, lock_path);
         for (int i = 0; i < 30; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        //阻塞
-                        countDownLatch.await();
-                        //获取锁
-                        lock.acquire();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
-                    String orderNo = sdf.format(new Date());
-                    System.out.println("生成的订单号是：" + orderNo);
-                    try {
-                        //释放锁
-                        lock.release();
-                    } catch (Exception e) {
-                    }
+            new Thread(() -> {
+                try {
+                    //阻塞
+                    countDownLatch.await();
+                    //获取锁
+                    lock.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+                String orderNo = sdf.format(new Date());
+                System.out.println("生成的订单号是：" + orderNo);
+                try {
+                    //释放锁
+                    lock.release();
+                } catch (Exception e) {
                 }
             }).start();
             //唤醒
